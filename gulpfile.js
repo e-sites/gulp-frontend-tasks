f27eef1921284dcc6165930d91fa7a55ea6b0b87
Boye Oomens <boye@e-sites.nl>
2014-12-10 10:47:57 +0100

    Reorganized the gulpfile a bit

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,36 @@
-'use strict';
+(function (global) {
 
-// Expose config, Gulp and some plugins that are used by multiple tasks
-global.conf = require('./assets/js/tasks/config.json');
-global.gulp = require('gulp');
-global.plumber = require('gulp-plumber');
-global.sourcemaps = require('gulp-sourcemaps');
+	'use strict';
 
-var requireDir = require('require-dir'),
-	notify = require('gulp-notify'),
-	dir = requireDir(conf.path.tasks);
+	// Expose config, Gulp and some plugins that are used by multiple tasks
+	global.conf = require('./assets/js/tasks/config.json');
+	global.gulp = require('gulp');
+	global.plumber = require('gulp-plumber');
+	global.sourcemaps = require('gulp-sourcemaps');
 
-global._plumbError = function (taskName, msg) {
-	return plumber({
-		errorHandler: notify.onError({
+	var requireDir = require('require-dir'),
+		notify = require('gulp-notify');
+
+	global._plumbError = function (taskName, msg) {
+		return plumber({
+			errorHandler: notify.onError({
+				title: taskName,
+				message: 'Error: ' + msg
+			})
+		});
+	};
+
+	global._notifySuccess = function (taskName, msg) {
+		return notify({
 			title: taskName,
-			message: 'Error: ' + msg
-		})
-	});
-};
+			message: 'Success: ' + msg
+		});
+	};
 
-global._notifySuccess = function (taskName, msg) {
-	return notify({
-		title: taskName,
-		message: 'Success: ' + msg
-	});
-};
+	// Load all tasks
+	requireDir(conf.path.tasks);
 
-// Default task when run with 'gulp'
-gulp.task('default', ['glue', 'svg', 'svg2png', 'less', 'js']);
+	// Default task when run with 'gulp'
+	gulp.task('default', ['glue', 'svg', 'svg2png', 'less', 'js']);
+
+}(global));
